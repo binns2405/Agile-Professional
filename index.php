e76a4b5f1edb83fb50f430dc3cced4216f620960
SSKO101 <126966622+SSKO101@users.noreply.github.com>
2024-11-25 10:16:44 +0000

    Index Update & Spear Phising Page
    Updated more the the index page ready to be fleshed out with extra content.
    
    Started work on Phishing page.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>test</title>
+    <title>UCLANCYBER | Home</title>
     
     <!-- Styles and Fonts -->
     <link rel="icon" href="images/websiteicon.png" type="image/png">
@@ -30,46 +30,51 @@
             </a>
         </div>
         <ul>
-            <li><a href="index.html">Home</a></li>
-            <li><a href="Projects.html">Projects</a></li>
-            <li><a href="About.html">About Us</a></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="Projects.html">DDOS</a></li>
+            <li><a href="SpearPhising.html">Spear Phising</a></li>
         </ul>
     </nav>
 
     <!-- Overlay With Video -->
     <section class="black-section">
 		 <div class="image-overlay">
+		<a href="#FirstSection"><button>Learn More</button></a>
 		<img src="white.png" alt="Overlay Image">
-			 		<button class="startbutton">test</button>
+			 		
         </div>
         <video autoplay loop muted class="myVideo" style="width: 2500px; height: 2000px;">
             <source src="Videos/Free Static Glitch Screen - Red Retro TV.mp4" type="video/mp4">
             Your browser does not support the video tag.
         </video>
+		
+    </section>
+	
+		<!-- First Section (CyberSecurity) -->
+	    <section id="FirstSection" class="black-section">
+        <h2 class="Section-Title">What is CYBERSECURITY</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>	
+			<a href="#DDOS"><button>Learn about DDOS</button></a>
+			<a href="#SpearPhising"><button>Learn about Spear Phising</button></a>
     </section>
 
-    <!-- Red Section -->
-    <section class="red-section">
+    <!-- Second Section (Learn About DDOS) -->
+    <section id="DDOS" class="red-section">
         <h2 class="Section-Title">DDOS</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<button>Learn more about DDOS</button>
+		<a href="#SpearPhising"><button>Learn about Spear Phising</button></a>
     </section>
 	
-	<!-- Black Section -->
-	    <section class="black-section">
+	<!-- Third Section (Learn About Spear Phising) -->
+	    <section id="SpearPhising" class="black-section">
         <h2 class="Section-Title">Spear Phising</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<button>Learn more about Spear Phising</button>
+		<a href="#DDOS"><button>Learn about DDOS</button></a>
     </section>
 
-    <!-- Policy Bar -->
-    <div class="text-bar">
-        <a href="docs/privacypolicy.docx" download>PRIVACY POLICY</a>
-        <a href="docs/termsofservice.docx" download>TERMS OF SERVICE</a>
-        <a href="contact.html">CONTACT US</a>
-        <p>© 2023 Epsilon Studios</p>
-    </div>
-	
-	      <!------Cursors--->
+	<!------Cursors--->
       <div class="inner-cursor"></div>
       <div class="outer-cursor"></div>
 
